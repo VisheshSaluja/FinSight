7815cb2a57b2c21c5f469a7cc295dcce064a026b
Krithika Jain <krithikajain2000@gmail.com>
2025-04-23 17:56:15 -0400

    deleted folders

--- FinSight/frontend/finsight-ui/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { provideRouter } from '@angular/router';
-import { UploadComponent } from './components/upload.component';
-
-export const routes: Routes = [
-  { path: '', component: UploadComponent }
-];
